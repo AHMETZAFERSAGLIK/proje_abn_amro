465b3c2ac5971b1cafae20b83401043b0f13d11e
Ahmet Zafer SAĞLIK <69164622+AHMETZAFERSAGLIK@users.noreply.github.com>
2021-06-09 23:36:40 +0200

    Reading and Logging csv

--- challenge.py
+++ challenge.py
@@ -4,27 +4,7 @@ from pyspark.sql import SparkSession
 
 spark = SparkSession.builder.getOrCreate()
 sc = SparkContext.getOrCreate()
-COL_DICT = {
-    "id": "client_identifier",
-    "btc_a": "bitcoin_address",
-    "cc_t": "credit_card_type"
-}
 
-
-def filter_by_country(df, cont_1, cont_2):
-    """Filtering DataFrame according to two countries given as param.
-
-    "OR" operation has been used.Function returns both countries results.
-
-    :param dataframe object df: PySpark Dataframe object.
-    :param str cont_1: First country for filtering.
-    :param str cont_2: Second country for filtering.
-    :return: Dataframe that includes result of both countries.
-    :rtype: PySpark Dataframe Object
-
-
-    """
-    return df.filter((df.country == cont_1) | (df.country == cont_2))
 
 
 def read_csv(df_name, df2_name):
@@ -44,20 +24,6 @@ def read_csv(df_name, df2_name):
     return df, df_2
 
 
-def col_rename(col_dict, df_con_rename):
-    """Chancing name of the colums of Dataframe according to dict given as param.
-
-    :param dict col_dict: Dictionary which includes old and new names of columns.
-    :param DataFrame df_con_rename: Pyspark Dataframe object for column renaming.
-    :return: Dataframe that includes with new column renames.
-    :rtype: PySpark Dataframe Object
-
-
-    """
-
-    for ex, nex in col_dict.items():
-        df_con_rename = df_con_rename.withColumnRenamed(ex, nex)
-    return df_con_rename
 
 
 def main():
@@ -67,13 +33,7 @@ def main():
     df2_name = sys.argv[4]
     log4jLogger = sc._jvm.org.apache.log4j
     log4jLogger.LogManager.getLogger(__name__)
-    df, df_2 = read_csv(df_name, df2_name)
-    df_fil = filter_by_country(df, cont_1, cont_2)
-    df_fil = df_fil.drop("first_name", "last_name", "country")
-    df_2 = df_2.drop("cc_n")
-    df_con = df_fil.join(df_2, ['id'])
-    df_con_rename = col_rename(COL_DICT, df_con)
-    df_con_rename.toPandas().to_csv('./client_data/mycsv.csv')
+
 
 
 if __name__ == '__main__':
--- challenge_test.py
+++ challenge_test.py
@@ -18,34 +18,3 @@ def test_read():
     assert df.count() == 1000
     assert df_2.count() == 1000
 
-
-def test_filter():
-    """Test for Filtering dataframe.
-
-    Checks the function of filter_by_country by comparing with test Dataframe.
-
-    """
-
-    data = [("Netherlands",), ("United Kingdom",), ("France",)]
-    input_df = spark.createDataFrame(data, ["country"])
-    exp_data = [("Netherlands",), ("United Kingdom",)]
-    exp_input_df = spark.createDataFrame(exp_data, ["country"])
-    result_df = filter_by_country(input_df, "United Kingdom", "Netherlands")
-    assert_df_equality(result_df, exp_input_df)
-
-
-def test_rename():
-    """Test for Renaming dataframe.
-
-    Checks the function of col_rename by comparing with test Dataframe.
-
-    """
-
-    df = spark.createDataFrame(
-        [("", "", "", "")], ["id", "email", "btc_a", "cc_t"])
-    df_con_rename = col_rename(COL_DICT, df)
-    assert df_con_rename.columns == [
-        "client_identifier",
-        "email",
-        "bitcoin_address",
-        "credit_card_type"]
