22c350c6fb864f5fa63b3aff62a55f95f2226632
Ahmet Zafer SAĞLIK <69164622+AHMETZAFERSAGLIK@users.noreply.github.com>
2021-06-11 00:23:47 +0200

    Setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='proje_abn_amro',
+    version='0.0.1',
+    packages=[''],
+    url='https://github.com/AHMETZAFERSAGLIK/proje_abn_amro',
+    license='',
+    author='zafer',
+    author_email='zafersaglk@gmail.com',
+    description='Challenge for Abn Amro'
+)
